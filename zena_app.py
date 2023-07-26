840f9b24cae4b3833367e495c3d18c3de5db2f1c
sasheppard <134110984+sasheppard@users.noreply.github.com>
2023-07-26 17:22:32 -0600

    Update zena_app.py
    add display logic

--- zena_app.py
+++ zena_app.py
@@ -1,12 +1,35 @@
 import streamlit
 import snowflake.connector
+import pandas as pd
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('Zena\'s Amazing Athleisure Catalog')
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+
+my_cur.execute("SELECT color_or_style from catalog_for_website")
+my_catalog = my_cur.fetchall()
+
+df = pd.DataFrame(my_catalog)
+
+color_list = df[0].values.tolist()
+option = streamlit.selectbox('Pick a sweatsuit color or style:', list(color_list))
+
+product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!'
+
+my_cur.execute("SELECT direct_url, price, size_list, upsell_product_desc FROM catalog_for_website
+               WHERE color_or_style = "' + option + '";")
+df2 = my_cur.fetchone()
+
+streamlit.image(
+        df2[0],
+        width=400,
+        caption=product_caption
+    )
+
+streamlit.write('Price: ' df2[1])
+streamlit.write(Sizes Available: ', df2[2])
+streamlit.write(df2[3])
+
+        
 
