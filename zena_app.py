d4b66c52c4d901ec49ab755e5e284b3b6ad8eeb3
sasheppard <134110984+sasheppard@users.noreply.github.com>
2023-07-26 17:05:53 -0600

    Update zena_app.py

--- zena_app.py
+++ zena_app.py
@@ -3,7 +3,7 @@ import snowflake.connector
 
 streamlit.title('My Parents New Healthy Diner')
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["SNOWFLAKE"])
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
